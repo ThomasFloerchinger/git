41ef196536af6a98bb922bb4a5ec1d5cde05c2d3
Thomas Floerchinger <thomas_floerchinger@web.de>
2021-06-14 12:14:52 +0200

    psuh: add man page and usage
    Signed-off-by: Thomas Floerchinger <thomas_floerchinger@web.de>

--- builtin/psuh.c
+++ builtin/psuh.c
@@ -3,7 +3,13 @@
 #include "wt-status.h"
 #include "commit.h"
 #include "pretty.h"
+#include "parse-options.h"
 
+static const char * const psuh_usage[] = {
+					  N_("git psuh[<arg>...]"),
+					  NULL,
+};
+				      
 int cmd_psuh(int argc, const char **argv, const char *prefix)
 {
   int i;
@@ -12,6 +18,12 @@ int cmd_psuh(int argc, const char **argv, const char *prefix)
   struct commit *c = NULL;
   struct strbuf commitline = STRBUF_INIT;
 
+  struct option options[] = {
+			     OPT_END()
+  };
+
+  argc = parse_options(argc, argv, prefix, options, psuh_usage, 0);
+  
   printf(_("Pony saying hello goes here.\n"));
 
   printf(Q_("Your args (there is %d):\n",
